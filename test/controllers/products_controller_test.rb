ca2cbcebfd7df94ea92fd2bd32ffd7b2d471d034
IvanDanyliv <danyliv04@gmail.com>
2019-05-01 22:18:27 +0300

    first commit

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,19 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+        title:       'Lorem Ipsum',
+        description: 'Wibbles are fun!',
+        image_url:   'lorem.jpg',
+        price:       19.95
+    }
+  end
+
+  test "should create product" do
+    assert_difference('Product.count') do
+      post products_url, params: { product: @update }
+    end
+    assert_redirected_to product_url(Product.last)
   end
 
   test "should get index" do
@@ -15,13 +28,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create product" do
-    assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    end
-
-    assert_redirected_to product_url(Product.last)
-  end
 
   test "should show product" do
     get product_url(@product)
@@ -34,8 +40,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
+  end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', -1) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
